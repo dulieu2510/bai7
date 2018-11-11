ddc9ed35681847fec087676e0a1842901428996c
dulieu2510 <giaptg@gmail.com>
2018-11-11 15:29:12 +0700

    lam viec voi translate di chuyen image view

--- app/src/main/java/a20181110/bai7/MainActivity.java
+++ app/src/main/java/a20181110/bai7/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
    Button bt1;
@@ -15,12 +18,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt1 = (Button) findViewById(R.id.bt);
+        ImageView img1 = (ImageView) findViewById(R.id.img1);
+        final Animation ani = AnimationUtils.loadAnimation(this,R.anim.animation_translate);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(ani);
+            }
+        });
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intt = new Intent(MainActivity.this,trang2.class );
+
                // intt.putExtra("ten","chuoi mac dinh");
                 startActivity(intt);
+               overridePendingTransition(R.anim.vao,R.anim.ra);
             }
         });
     }
